d71c9a6c0ec6774dde33222f2df6ae0477ce40ab
itschool <itschool@tou.edu.kz>
2024-10-06 23:10:38 +0500

    init

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let a = 100;
+const b = "Hello";
+
+// typeof a;
+// typeof(b);
+
+// alert(typeof a);
+
+/*
+    let c;
+    c = typeof(b);
+    alert(c);
+*/
+
+// console.log(typeof(a));
+
+// let d = typeof b;
+// console.log(d);
+
+// let string1 = "Hello world!";
+// console.log(string1);
+
+// let string2 = `string1 = ${string1}`;
+// console.log(string2);
+// "2";
+// console.log(typeof String(2));
+
+// console.log(Number("123a"));
+
+let result = prompt("Hello world!");
+console.log(result);
